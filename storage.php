b9265180a62f7491fc3561e3c28aef410d34bb0e
GabrielOrtisHelfer <143913074+gabrielortishelfer@users.noreply.github.com>
2024-02-27 20:36:00 -0300

    .
    add no arquivo storage.php 5<p>(PRODUTOS)

--- storage.php
+++ storage.php
@@ -24,6 +24,11 @@
         <!-- Formulário de login -->
         <form action="auth.php" method="post">
             <h3> Storage </h3>
+            <p>Camiseta: R$30,00       QTD:10  </p> 
+            <p>Ventilador: R$ 160,00      QTD:20 </p>  
+            <p>Monitor: R$ 300,00       QTD:15 </p>   
+            <p>Teclado: R$: 130       QTD:10 </p>
+       
             <a href="storage.php"><button type="button">Full View</button></a>
         </form>
         
